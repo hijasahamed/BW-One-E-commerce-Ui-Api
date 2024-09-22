31a26bf1f07ddc6ff4909a3a4d8e7e17d431f561
hijas <hijasahamed2000@gmail.com>
2024-09-22 17:21:33 +0530

    notification screen app bar done

--- lib/view/notifications_screen/notifications_screen.dart
+++ lib/view/notifications_screen/notifications_screen.dart
@@ -1,4 +1,7 @@
+import 'package:bw_week_one_e_commerce/model/colors.dart';
+import 'package:bw_week_one_e_commerce/model/textwidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class NotificationsScreen extends StatelessWidget {
   const NotificationsScreen({super.key,required this.screenSize});
@@ -6,8 +9,19 @@ class NotificationsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(child: Text('Notifications'),),
+      backgroundColor: appbackgroundWhite,
+      appBar: AppBar(
+        backgroundColor: appbackgroundWhite,
+        leading: GestureDetector(
+          onTap: () => Navigator.of(context).pop(),
+          child: Padding(
+            padding: EdgeInsets.all(screenSize.width/35),
+            child: SvgPicture.asset('assets/images/green back button.svg',fit: BoxFit.cover,),
+          ),
+        ),
+        title: TextWidget(text: 'Notifications', color: textGreyColor, fontStyle: 'QuickSand', size: screenSize.width/19, weight: FontWeight.bold),
+      ),
+      body: const Center(child: Text('Notifications'),),
     );
   }
 }
