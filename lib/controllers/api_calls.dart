c2a010cb6c457f7679375c77cdb159d19483bc88
hijas <hijasahamed2000@gmail.com>
2024-09-24 15:26:39 +0530

    isolates used for notification fetching

--- lib/controllers/api_calls.dart
+++ lib/controllers/api_calls.dart
@@ -1,8 +1,15 @@
 import 'dart:convert';
 import 'package:bw_week_one_e_commerce/models/api_model.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
+
+// isolates used for multi threading
 Future<List<NotificationsApiModel>> fetchNotifications() async {
+  return await compute(fetchNotificationsInIsolate, null);
+}
+
+Future<List<NotificationsApiModel>> fetchNotificationsInIsolate(_) async {
   const url = 'https://raw.githubusercontent.com/shabeersha/test-api/main/test-notifications.json';
   final response = await http.get(Uri.parse(url));
 
@@ -14,57 +21,3 @@ Future<List<NotificationsApiModel>> fetchNotifications() async {
     throw Exception('Failed to load notifications');
   }
 }
-
-// Future<List<NotificationsApiModel>> fetchNotifications() async {
-//   const url =
-//       'https://raw.githubusercontent.com/shabeersha/test-api/main/test-notifications.json';
-//   final response = await http.get(Uri.parse(url));
-
-//   if (response.statusCode == 200) {
-//     final jsonData = json.decode(response.body);
-//     final List notificationsData = jsonData['data'];
-
-//     // Convert the raw JSON to a list of NotificationsApiModel objects
-//     List<NotificationsApiModel> notificationsList = notificationsData
-//         .map((json) => NotificationsApiModel.fromJson(json))
-//         .toList();
-
-//     // Sort the list by timestamp (latest first)
-//     notificationsList.sort((a, b) =>
-//         DateTime.parse(b.timestamp).compareTo(DateTime.parse(a.timestamp)));
-
-//     return notificationsList;
-//   } else {
-//     throw Exception('Failed to load notifications');
-//   }
-// }
-
-
-
-// isloates added in fetching function
-
-// List<NotificationsApiModel> sortNotificationsInIsolate(List<NotificationsApiModel> notifications) {
-//   notifications.sort((a, b) => DateTime.parse(b.timestamp).compareTo(DateTime.parse(a.timestamp)));
-//   return notifications;
-// }
-
-
-// Future<List<NotificationsApiModel>> fetchNotifications() async {
-//   const url = 'https://raw.githubusercontent.com/shabeersha/test-api/main/test-notifications.json';
-//   final response = await http.get(Uri.parse(url));
-
-//   if (response.statusCode == 200) {
-//     final jsonData = json.decode(response.body);
-//     final List notificationsData = jsonData['data'];
-
-//     // Convert the raw JSON to a list of NotificationsApiModel objects
-//     List<NotificationsApiModel> notificationsList = notificationsData
-//         .map((json) => NotificationsApiModel.fromJson(json))
-//         .toList();
-
-//     // Use isolate to sort the notifications (offload sorting task)
-//     return await compute(sortNotificationsInIsolate, notificationsList);
-//   } else {
-//     throw Exception('Failed to load notifications');
-//   }
-// }
